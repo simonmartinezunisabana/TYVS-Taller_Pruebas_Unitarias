060a354f1b2c95de95e5d916d46d525e54e04836
SimonMartinez2117 <simonmaga@unisabana.edu.co>
2026-09-06 21:43:40 -0500

    feat: reject duplicated id using an id memory set (GREEN)

--- registraduria/src/main/java/edu/unisabana/tyvs/domain/service/Registry.java
+++ registraduria/src/main/java/edu/unisabana/tyvs/domain/service/Registry.java
@@ -1,5 +1,8 @@
 package edu.unisabana.tyvs.domain.service;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import edu.unisabana.tyvs.domain.model.Person;
 import edu.unisabana.tyvs.domain.model.RegisterResult;
 
@@ -16,6 +19,8 @@ import edu.unisabana.tyvs.domain.model.RegisterResult;
  */
 public class Registry {
 
+    private final Set<Integer> usedIds = new HashSet<>();
+
     public RegisterResult registerVoter(Person p) {
         if (p == null || p.getId() <= 0) {
             return RegisterResult.INVALID; // regla defensiva
@@ -28,6 +33,11 @@ public class Registry {
         }
         if (p.getAge() < 18) {
             return RegisterResult.UNDERAGE;
+        }
+        if(usedIds.contains(p.getId())) {
+            return RegisterResult.DUPLICATED;
+        }else {
+            usedIds.add(p.getId());
         }
 
         // TODO iteracion 3 en adelante: validar duplicados.
